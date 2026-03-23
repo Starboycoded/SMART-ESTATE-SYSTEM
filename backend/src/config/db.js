6bbbf2ef1e19852fff973d483e2e6401dec48e46
itunuolaaa <itorepoitunu@gmail.com>
2026-03-23 18:00:44 +0100

    .env

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "orepoitunuola",
-  database: "smart_estate",
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "orepoitunuola",
+  database: process.env.DB_NAME || "smart_estate",
 });
 
 module.exports = db;
